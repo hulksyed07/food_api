d2f782042bc4b6eb7e85d9a76ebb40b07cc05745
hulksyed07 <hulk.syed07@gmail.com>
2017-09-17 04:51:02 +0530

    Temporary hack to allow interaction between angular app and this api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+	before_action :allow_cross_domain_interaction
+
+	private
+	
+	def allow_cross_domain_interaction
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+		headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+		headers['Access-Control-Max-Age'] = "1728000"
+	end
 end
