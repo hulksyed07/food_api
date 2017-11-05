50952b401b7b64cd7dd172c9da089d9071dab898
hulksyed07 <hulk.syed07@gmail.com>
2017-11-06 01:18:20 +0530

    added content-type validation as required from rails 4 onwards

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
